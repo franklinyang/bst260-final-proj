024416aaf71104a70be58a5a4ac8d38f019cd36b
genevievelyons <glyons@hsph.harvard.edu>
2019-11-23 14:49:35 -0500

    finish regression hospital table

--- code/02_Regression_Master_Table_Build.R
+++ code/02_Regression_Master_Table_Build.R
@@ -33,7 +33,7 @@ library(openintro)
 
 #setwd("/Users/genevievelyons/Intro to DS/bst260-final-proj/code")
 con <- dbConnect(RSQLite::SQLite(), "../database/db.sqlite")
-
+dbListTables(con)
 
 ####################
 ####################
@@ -108,6 +108,7 @@ hospital_info$Responsiveness_of_Hospital_Staff_Performance_Rate <- as.numeric(ho
 
 #Pull deaths and complications data 
 deaths <- dbFetch(dbSendQuery(con, "select * from outcomes_complications_and_deaths"))
+#View(deaths)
 
 # Categorize: Complications/Deaths are stemming from the HOSPITAL (e.g., post-surgical complication), or from PATIENTS (e.g., death from stroke)
 deaths %>% select(Measure_Name) %>% distinct()
@@ -171,11 +172,34 @@ deaths_join <- left_join(left_join(any_complications_score, hospital_level_compl
 #Link it up with hospital info
 hospital_info <- left_join(hospital_info, deaths_join, by = "Facility_ID") 
 
-#Fix the NaN's
-hospital_info$any_complications_score[is.nan(hospital_info$any_complications_score) == T] <- NA_character_
-hospital_info$hospital_level_complications_score[is.nan(hospital_info$hospital_level_complications_score) == T] <- NA_character_
-hospital_info$patient_level_complications_score[is.nan(hospital_info$patient_level_complications_score) == T] <- NA_character_
 
+####################
+#Hospital Acquired Conditions
+####################
+
+#View(dbFetch(dbSendQuery(con, "select * from outcomes_hac_reductions")))
+hac <- dbFetch(dbSendQuery(con, "select * from outcomes_hac_reductions"))
+hac <- hac %>% select(Facility_ID, TOTAL_HAC_SCORE, PAYMENT_REDUCTION) %>% rename(payment_reduction_hac = PAYMENT_REDUCTION)
+
+#join
+hospital_info <- left_join(hospital_info, hac, by = "Facility_ID")
+
+####################
+#Readmissions
+####################
+
+readmissions <- dbFetch(dbSendQuery(con, "select * from outcomes_readmissions_reductions"))
+#readmissions %>% select(Measure_Name) %>% distinct()
+#View(readmissions)
+
+#Aggregate the excess readmissons
+readmissions <- readmissions %>% group_by(Facility_ID) %>% 
+  mutate(excess_readmissions = sum(na.omit(as.numeric(Excess_Readmission_Ratio) * as.numeric(Number_of_Discharges)))/sum(na.omit(as.numeric(Number_of_Discharges)))) %>%
+  select(Facility_ID, excess_readmissions) %>%
+  distinct()
+
+#Join 
+hospital_info <- left_join(hospital_info, readmissions, by = "Facility_ID")
 
 ####################
 #Healthcare Policy Focused States (IV: Medicaid Expansion)
@@ -210,12 +234,17 @@ census_data <- dbFetch(dbSendQuery(con,
 
 #View(census_data)
 
-#Clean up the County/State Name#
+#Clean up the County/State Name
   #Pull the county/state names
 census_data$county <- unlist(str_split(census_data$geography, ', ', simplify = T))[,1]
 census_data$State <- unlist(str_split(census_data$geography, ', ', simplify = T))[,2]
-  #Get rid of "county"
+  #Get rid of "county", parish, etc.
 census_data$county <- gsub(" County","",census_data$county)
+census_data$county <- gsub(" Parish","",census_data$county)
+census_data$county <- gsub(" Municipality","",census_data$county)
+census_data$county <- gsub(" Borough","",census_data$county)
+census_data$county <- gsub(" Census Area","",census_data$county)
+census_data$county <- gsub("'","",census_data$county)
   #Abbrevate states
 census_data$State <- state2abbr(census_data$State)
   #Create Uppercase version of county for joining to hospital info
@@ -223,9 +252,24 @@ census_data$County_Name <- toupper(census_data$county)
   #Fix County Names that are different in census vs hospital data 
 census_data$County_Name <- ifelse(census_data$County_Name == "DEKALB", "DE KALB", census_data$County_Name)
 census_data$County_Name <- ifelse(census_data$County_Name == "DESOTO", "DE SOTO", census_data$County_Name)
-
-#DE KALB -> DEKALB in census data
-#DE SOTO -> DESOTO in census data
+census_data$County_Name <- ifelse(census_data$County_Name == "DUPAGE", "DU PAGE", census_data$County_Name)
+census_data$County_Name <- ifelse(census_data$County_Name == "EAST BATON ROUGE", "E. BATON ROUGE", census_data$County_Name)
+census_data$County_Name <- ifelse(census_data$County_Name == "JEFFERSON DAVIS", "JEFFRSON DAVIS", census_data$County_Name)
+census_data$County_Name <- ifelse(census_data$County_Name == "LAPORTE", "LA PORTE", census_data$County_Name)
+census_data$County_Name <- ifelse(census_data$County_Name == "NORTHUMBERLAND", "NORTHUMBERLND", census_data$County_Name)
+census_data$County_Name <- ifelse(census_data$County_Name == "SCOTTS BLUFF", "SCOTT BLUFF", census_data$County_Name)
+census_data$County_Name <- ifelse(census_data$County_Name == "DISTRICT OF COLUMBIA", "THE DISTRICT", census_data$County_Name)
+census_data$County_Name <- ifelse(census_data$County_Name == "MCCRACKEN", "MC CRACKEN", census_data$County_Name)
+census_data$County_Name <- ifelse(census_data$County_Name == "MCDONOUGH", "MC DONOUGH", census_data$County_Name)
+census_data$County_Name <- ifelse(census_data$County_Name == "MCDOWELL", "MC DOWELL", census_data$County_Name)
+census_data$County_Name <- ifelse(census_data$County_Name == "MCDUFFIE", "MC DUFFIE", census_data$County_Name)
+census_data$County_Name <- ifelse(census_data$County_Name == "MCHENRY", "MC HENRY", census_data$County_Name)
+census_data$County_Name <- ifelse(census_data$County_Name == "MCKEAN", "MC KEAN", census_data$County_Name)
+census_data$County_Name <- ifelse(census_data$County_Name == "MCLEAN", "MC LEAN", census_data$County_Name)
+census_data$County_Name <- ifelse(census_data$County_Name == "MCLENNAN", "MC LENNAN", census_data$County_Name)
+census_data$County_Name <- ifelse(census_data$County_Name == "MCLEOD", "MC LEOD", census_data$County_Name)
+census_data$County_Name <- ifelse(census_data$County_Name == "MCMINN", "MC MINN", census_data$County_Name)
+census_data$County_Name <- ifelse(census_data$County_Name == "DO<F1>A ANA", "DONA ANA", census_data$County_Name)
 
   #QC: compare counties in one vs the other
   hospital_info$County_Name[c(hospital_info$County_Name,'-', hospital_info$State) %in% c(census_data$County_Name,'-', census_data$State) == F] %>% length()
@@ -233,33 +277,101 @@ census_data$County_Name <- ifelse(census_data$County_Name == "DESOTO", "DE SOTO"
   #many are N/A?
   sum(is.na(hospital_info$County_Name) == T) #0. OK those are fine.
   #look at them 
-  hospital_info[c(hospital_info$County_Name,'-', hospital_info$State) %in% c(census_data$County_Name,'-', census_data$State) == F,] %>% View()
-  hospital_info$County_Name[c(hospital_info$County_Name,'-', hospital_info$State) %in% c(census_data$County_Name,'-', census_data$State) == F] %>% View()
-  
-  
-  
+  hospital_info[hospital_info$State %in% census_data$State == T & c(hospital_info$County_Name,'-', hospital_info$State) %in% c(census_data$County_Name,'-', census_data$State) == F,] #%>% View()
+  #All fixed. Good!
+  #census_data %>% select(State) %>% distinct() %>% View()
 
 #join to hospital_info
+hospital_info <- left_join(hospital_info, census_data, by = c("County_Name","State"))
 
 
 ####################
 #Hospital density in the county (# hospitals in county; # hospitals per capita in the county)
 ####################
 
+hospital_info <- hospital_info %>% 
+  group_by(County_Name, State) %>%
+  mutate(hospital_density = length(Facility_ID),
+         hospital_density_per_100k_capita = length(Facility_ID)*100000 / as.numeric(pop_census_2017))
+  
+  #hospital_info %>% filter(County_Name == "MIAMI-DADE" & State == "FL") %>% View()
 
 ####################
 #Region (northeast, northwest, south, etc.)
 ####################
 
+library(dslabs)
+data(murders)
+murders <- murders %>% select(abb,region) %>% rename(State = abb)
 
-
-
-
+hospital_info <- left_join(hospital_info, murders, by = "State")
 
 
 ###########
-#Write Table, Disconnect from DB
+#Finish fields, Write Table, Disconnect from DB
 ###########
+  
+#exclude PR, Guam, AS, and other non-states 
+hospital_info %>% filter(State %in% census_data$State == F) %>% dim() #excluding 57 hospitals
+
+hospital_info <- hospital_info %>% ungroup
+hospital_info <- hospital_info %>% filter(State %in% census_data$State == T) 
+#3249 records; 3306-57 = 3249. Good.
+
+#exclude tribal hospitals - low complications because they have to move anyone with complications to a larger hospital
+hospital_info %>% ungroup %>% select(Hospital_Ownership) %>% distinct()
+hospital_info <- hospital_info %>% filter(Hospital_Ownership != "Tribal") #3244 hospitals
+
+#Fix the NaN's
+hospital_info$any_complications_score[is.nan(hospital_info$any_complications_score) == T] <- NA_character_
+hospital_info$hospital_level_complications_score[is.nan(hospital_info$hospital_level_complications_score) == T] <- NA_character_
+hospital_info$patient_level_complications_score[is.nan(hospital_info$patient_level_complications_score) == T] <- NA_character_
+hospital_info$excess_readmissions[is.nan(hospital_info$excess_readmissions) == T] <- NA_character_
+
+#fix classes of variables
+class(hospital_info$ip_spend)
+class(hospital_info$op_spend)
+class(hospital_info$total_spend)
+class(hospital_info$Responsiveness_of_Hospital_Staff_Performance_Rate)
+class(hospital_info$total_performance_score_patient_experience)
+class(hospital_info$any_complications_score)#chr
+class(hospital_info$hospital_level_complications_score)#chr
+class(hospital_info$patient_level_complications_score)#chr
+class(hospital_info$TOTAL_HAC_SCORE)#chr
+class(hospital_info$payment_reduction_hac)#chr
+class(hospital_info$excess_readmissions)#chr
+class(hospital_info$hc_policy_focused_state)#num
+class(hospital_info$median_household_income)#chr
+class(hospital_info$mean_household_income)#chr
+class(hospital_info$income_w_social_security)#chr
+class(hospital_info$income_w_retirement)#chr
+class(hospital_info$mean_retirement_income)#chr
+class(hospital_info$pop_denominator_healthinsurance)#chr
+class(hospital_info$pop_with_healthinsurance)#chr
+class(hospital_info$pop_no_healthinsurance)#chr
+class(hospital_info$perc_pop_below_poverty)#chr
+class(hospital_info$pop_census_2017)#chr
+class(hospital_info$hospital_density)
+class(hospital_info$hospital_density_per_100k_capita)
+
+
+hospital_info$hc_policy_focused_state <- as.factor(hospital_info$hc_policy_focused_state)
+hospital_info$any_complications_score <- as.numeric(hospital_info$any_complications_score)
+hospital_info$hospital_level_complications_score <- as.numeric(hospital_info$hospital_level_complications_score)
+hospital_info$patient_level_complications_score <- as.numeric(hospital_info$patient_level_complications_score)
+hospital_info$TOTAL_HAC_SCORE <- as.numeric(hospital_info$TOTAL_HAC_SCORE)
+hospital_info$payment_reduction_hac <- as.numeric(hospital_info$payment_reduction_hac)
+hospital_info$excess_readmissions <- as.numeric(hospital_info$excess_readmissions)
+hospital_info$median_household_income <- as.numeric(hospital_info$median_household_income)
+hospital_info$mean_household_income <- as.numeric(hospital_info$mean_household_income)
+hospital_info$income_w_social_security <- as.numeric(hospital_info$income_w_social_security)
+hospital_info$income_w_retirement <- as.numeric(hospital_info$income_w_retirement)
+hospital_info$mean_retirement_income <- as.numeric(hospital_info$mean_retirement_income)
+hospital_info$pop_denominator_healthinsurance <- as.numeric(hospital_info$pop_denominator_healthinsurance)
+hospital_info$pop_with_healthinsurance <- as.numeric(hospital_info$pop_with_healthinsurance)
+hospital_info$pop_no_healthinsurance <- as.numeric(hospital_info$pop_no_healthinsurance)
+hospital_info$perc_pop_below_poverty <- as.numeric(hospital_info$perc_pop_below_poverty)
+hospital_info$pop_census_2017 <- as.numeric(hospital_info$pop_census_2017)
   
 # Make all columns lowercase
 names(hospital_info) <- tolower(names(hospital_info))
@@ -267,6 +379,7 @@ names(hospital_info) <- tolower(names(hospital_info))
 dbRemoveTable(con, "master_hospital_table", hospital_info)
 dbWriteTable(con, "master_hospital_table", hospital_info)
 
-dbFetch(dbSendQuery(con, "select * from master_hospital_table"))
+#View(dbFetch(dbSendQuery(con, "select * from master_hospital_table")))
 
 dbDisconnect(con)
+
