436d6c01780409c9464a103b9328e929b2822cc4
Franklin <franklin@firefly.health>
2019-12-04 09:36:23 -0500

    Cleaning up viz for country-level viz
    TODOs:
    - descriptions for each measure
    - incorporate new plots w/ confidence intervals
    - add in state-level drilldown

--- app.R
+++ app.R
@@ -8,15 +8,29 @@ require(openintro)
 
 # Begin connection
 con <- dbConnect(RSQLite::SQLite(), "database/db.sqlite")
+
+measure_options = c("Readmissions" = "excess_readmissions",
+                    "Post-operative complications" = "hospital_level_complications_score",
+                    "Responsiveness of hospital staff" = "responsiveness_of_hospital_staff_performance_rate",
+                    "In-patient spend" = "ip_spend",
+                    "Total spend" = "total_spend",
+                    "Policy-focused state" = "hc_policy_focused_state",
+                    "Hospital density" = "hospital_density_per_100k_capita",
+                    "Median household income" = "median_household_income")
 states_outcomes_data <- dbFetch(
   dbSendQuery(
     con,
     "
         SELECT
           state,
-          AVG(hospital_overall_rating) AS hospital_overall_rating,
-          AVG(total_performance_score_patient_experience) AS total_performance_score_patient_experience,
-          AVG(hospital_level_complications_score) AS hospital_level_complications_score
+          AVG(excess_readmissions) AS excess_readmissions,
+          AVG(hospital_level_complications_score) AS hospital_level_complications_score,
+          AVG(responsiveness_of_hospital_staff_performance_rate) AS responsiveness_of_hospital_staff_performance_rate,
+          AVG(ip_spend) AS ip_spend,
+          AVG(total_spend) AS total_spend,
+          AVG(hc_policy_focused_state) AS hc_policy_focused_state,
+          AVG(hospital_density_per_100k_capita) AS hospital_density_per_100k_capita,
+          AVG(median_household_income) AS median_household_income
         FROM master_hospital_table
         GROUP BY state
       "
@@ -30,6 +44,7 @@ facility_outcomes_data <- dbFetch(
           facility_id,
           total_performance_score_patient_experience,
           hospital_level_complications_score,
+          excess_readmissions,
 
           hc_policy_focused_state,
           median_household_income,
@@ -47,20 +62,25 @@ ui <- fluidPage(
   titlePanel("BST260 Final Project Visualizations"),
   tabsetPanel(
     tabPanel(
+      "Other visualizations"
+    ),
+    tabPanel(
       "Performance, Complication, Overall Scores by state",
-      selectInput(
-        'measure',
-        'Measure',
-        c('hospital_overall_rating', 'total_performance_score_patient_experience', 'hospital_level_complications_score'),
-      ),
-      plotOutput(outputId = "state_performance_map")
+      sidebarLayout(
+        sidebarPanel(
+          radioButtons("measure", "Select outcome measure:", measure_options)
+        ),
+        mainPanel(
+          plotOutput(outputId = "state_performance_map")
+        )
+      )
     ),
     tabPanel(
       "Performance and Complications against spending",
       selectInput(
         'pop_measure',
         'Measure',
-        c('total_performance_score_patient_experience', 'hospital_level_complications_score'),
+        c('excess_readmissions', 'hospital_level_complications_score'),
       ),
       selectInput(
         'pop_feature',
@@ -85,8 +105,23 @@ server <- function(input, output) {
 
     # Create the map
     ggplot(measures_map, aes(long, lat, group = group)) +
-      geom_polygon(aes(fill = !!as.symbol(input$measure)), color = "white")
-
+      ggtitle(names(which(measure_options == input$measure))) +
+      geom_polygon(aes(fill = !!as.symbol(input$measure)), color = "white") +
+      theme(
+        panel.background = element_blank(),
+        axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        legend.position="top",
+        legend.direction="horizontal",
+        legend.title=element_blank(),
+        plot.title = element_text(size=18, hjust = 0.5),
+        legend.key.width = unit(3, "cm")
+      ) + scale_color_gradient(low = "#132B43",
+                               high = "#56B1F7")
   })
   output$facility_performance_plot <- renderPlot({
     facility_outcomes_data %>%
