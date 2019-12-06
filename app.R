5035be69aee2ff4d0df41fccfa61d6fef6c6c52f
Franklin <franklin@firefly.health>
2019-12-05 19:24:54 -0500

    Updating second RShiny tab

--- app.R
+++ app.R
@@ -57,6 +57,24 @@ facility_outcomes_data <- dbFetch(
   )
 )
 
+state <- master %>%
+  group_by(state) %>%
+  summarize(region = max(na.omit(region)),
+            hc_policy_focused_state = max(na.omit(hc_policy_focused_state)),
+            ip_spend = mean(na.omit(ip_spend)),
+            op_spend = mean(na.omit(op_spend)),
+            total_spend = mean(na.omit(total_spend)),
+            hospital_level_complications_score = mean(na.omit(hospital_level_complications_score)),
+            excess_readmissions = mean(na.omit(excess_readmissions)),
+            hospital_density_per_100k_capita = mean(na.omit(hospital_density_per_100k_capita)),
+            income_cat = mean(na.omit(as.numeric(income_cat))),
+            responsiveness_of_hospital_staff_performance_rate = mean(na.omit(responsiveness_of_hospital_staff_performance_rate)) 
+  )
+
+state_measure_options = c(
+  "Readmissions" = "excess_readmissions",
+  "Responsiveness of hospital staff" = "responsiveness_of_hospital_staff_performance_rate"
+)
 # Plotting performance score / complication score against total spend and segmenting by star rating
 ui <- fluidPage(
   titlePanel("BST260 Final Project Visualizations"),
@@ -68,7 +86,7 @@ ui <- fluidPage(
       "Measures segmented by state",
       sidebarLayout(
         sidebarPanel(
-          radioButtons("measure", "Select outcome measure:", measure_options)
+          radioButtons("measure", "Select measure:", measure_options)
         ),
         mainPanel(
           plotOutput(outputId = "state_performance_map")
@@ -76,22 +94,15 @@ ui <- fluidPage(
       )
     ),
     tabPanel(
-      "Performance and Complications against spending",
-      selectInput(
-        'pop_measure',
-        'Measure',
-        c('excess_readmissions', 'hospital_level_complications_score'),
-      ),
-      selectInput(
-        'pop_feature',
-        'Population feature',
-        c('median_household_income',
-          'perc_pop_below_poverty',
-          'pop_with_healthinsurance',
-          'total_spend'
-          ),
-      ),
-      plotOutput(outputId = "facility_performance_plot")
+      "Total spend as a function of measures",
+      sidebarLayout(
+        sidebarPanel(
+          radioButtons("state_measure", "Select measure:", state_measure_options)
+        ),
+        mainPanel(
+          plotOutput(outputId = "facility_performance_plot")
+        )
+      )
     )
   )
 )
@@ -118,18 +129,15 @@ server <- function(input, output) {
         legend.position="top",
         legend.direction="horizontal",
         legend.title=element_blank(),
-        plot.title = element_text(size=18, hjust = 0.5),
+        plot.title=element_text(size=18, hjust = 0.5),
         legend.key.width = unit(3, "cm")
       ) + scale_color_gradient(low = "#132B43",
                                high = "#56B1F7")
   })
   output$facility_performance_plot <- renderPlot({
-    facility_outcomes_data %>%
-        ggplot(aes(!!as.symbol(input$pop_feature),
-                   !!as.symbol(input$pop_measure),
-                   color=!!as.symbol(input$pop_feature))) +
-        geom_point() +
-        facet_grid(rows=vars(hc_policy_focused_state))
+    state %>% ggplot() + 
+      geom_point(aes(!!as.symbol(input$state_measure), total_spend, color = region, size = income_cat)) +
+      geom_smooth(aes(!!as.symbol(input$state_measure), total_spend))
   })
 }
 
